ab9c9309b3cd7580fc4ec050bd70a047e819d6e1
P4BLOll <pablosousa386@gmail.com>
2025-03-11 22:26:23 -0300

    Oficina de Java SpringBoot

---
+++ trinity-main/framework/src/main/java/br/com/etec/TrinityApplication.java
@@ -0,0 +1,13 @@
+package br.com.etec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinityApplication.class, args);
+	}
+
+}
